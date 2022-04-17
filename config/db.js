c7f361cfed411e9e14c16c12df7e536d33e85f33
kushagra kamal <kushagrakamal98@gmail.com>
2022-04-17 16:44:01 +0530

    a

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
 module.exports = ()=>{
-     return mongoose.connect("mongodb+srv://navneetfw14:navnee7250@cluster0.iowwj.mongodb.net/the_final?authSource=admin&replicaSet=atlas-o7alyc-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true")
+     return mongoose.connect("mongodb+srv://user:pass@cluster0.4xk9u.mongodb.net/fullstackproject?retryWrites=true&w=majority")
 }
